b2271d3bd55a1266f5c72a3df9e02b25c4bc2bf4
abdul0398 <abdulrkhan570@gmail.com>
2024-04-17 14:05:32 +0530

    Fix Heading

--- src/app/components/Dashboard/Main.tsx
+++ src/app/components/Dashboard/Main.tsx
@@ -5,18 +5,21 @@ import UnitTable from "../table/Main";
 
 export default function Dashboard() {
     return (
-        <div className="w-3/4 mx-auto py-10 px-4 bg-[#f3f4f6]">
-            <section className="w-full text-center mb-5">
-                <h2 className="text-2xl">URA Developer Sales Data (Units Launched and Sold)</h2>
-            </section>
-            <section className="flex w-full h-96 my-5">
-                <UnitTable />
-                <MonthDetail />
-            </section>
-            <section>
-                <UnitSold/>
-                <UnitLaunched/>
-            </section>
+        <div className="w-3/4 mx-auto py-10 px-4 ">
+                <section className="w-full mb-5">
+                    <h2 className="text-3xl">URA Developer Sales Data (Units Launched and Sold)</h2>
+                </section>
+            <div className="w-full h-full bg-[#f3f4f6]">
+
+                <section className="flex w-full h-96 my-5">
+                    <UnitTable />
+                    <MonthDetail />
+                </section>
+                <section>
+                    <UnitSold />
+                    <UnitLaunched />
+                </section>
+            </div>
         </div>
     )
 }
