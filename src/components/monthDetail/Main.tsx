3f827250c0d71b217b04afbdd88ad85907d6d612
abdul0398 <abdulrkhan570@gmail.com>
2024-05-23 17:27:11 +0530

    feaut: added sorting feaut in table

--- src/components/monthDetail/Main.tsx
+++ src/components/monthDetail/Main.tsx
@@ -140,7 +140,7 @@ export default function MonthDetail() {
             Units Launched in Report Month ({selectedMonth})
           </h1>
           <div className="min-w-full text-left text-xs font-light overflow-hidden">
-            <div className=" mt-5 border-b font-medium dark:border-neutral-500 grid gap-1 grid-cols-[15%_14%_10%_10%_10%_15%_10%_10%] text-xs">
+            <div className="mt-5 border-b font-medium dark:border-neutral-500 grid gap-1 grid-cols-[15%_14%_10%_10%_10%_15%_10%_10%] text-xs">
               <div className="px-1 text-xs flex">
                 Project
                 <span className="flex items-center ms-2">
@@ -206,7 +206,7 @@ export default function MonthDetail() {
                 </span>
               </div>
             </div>
-            <div className="overflow-hidden mt-">
+            <div className="overflow-hidden mt-5">
               <List
                 height={700}
                 itemCount={allListings.length}
